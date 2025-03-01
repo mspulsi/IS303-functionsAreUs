790b8a18386ff174e936eeca6e34c93575f98fb5
mspulsi <mspulsipher@gmail.com>
2025-02-28 20:18:00 -0700

    fixing the print message in the start function

--- functions.py
+++ functions.py
@@ -2,12 +2,7 @@
 def start(): 
     userName = input("What is your name?: ")
     print(f"Welcome {userName}!")
-    print("How this game works:"
-          "\n1) It asks you for the name of your team and the total number of games your team will play this season\n"
-          "2) it asks for the name of the opponents for each game.\n"
-          "3) The game will then randomly generate the scores for each of those games\n"
-          "4) It will display the scores generated for those games along with the home and opposing team names\n"
-          "5) It displays a win-loss record and tells you how your team did! (needs improvement, qualified for NCAA tournament, etc.)")
+    print("How this game works:\n1) It asks you for the name of your team and the total number of games your team will play this season\n2) it asks for the name of the opponents for each game.\n3) The game will then randomly generate the scores for each of those games\n4) It will display the scores generated for those games along with the home and opposing team names\n5) It displays a win-loss record and tells you how your team did! (needs improvement, qualified for NCAA tournament, etc.)")
     
 def selectTeams(listTeams = None):
 
